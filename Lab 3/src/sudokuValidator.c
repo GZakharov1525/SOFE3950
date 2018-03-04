166d6350c68814668eadd7e207ac818a955e8a99
GZakharov1525 <GZakharov1525@users.noreply.github.com>
2018-03-04 13:14:28 -0500

    Extract data from input file
    Finished setting up the 2D array of values that are to be read in from a file.

--- Lab 3/src/sudokuValidator.c
+++ Lab 3/src/sudokuValidator.c
@@ -10,7 +10,30 @@ int main(int argc, char* argv[])
 		printf("Could not open solution file.");
 	}
 
+	// Prep 2D array for incoming sudoku solution
+	int sudoku[9][9];
 
+	for (int row = 0; row <= 8; row++)
+	{
+		for (int column = 0; column <= 8; column++)
+		{
+			// Each digit is a character in the text file
+			// Need to convert to digit
+			char charFromFile = fgetc(inputFile);
+			int intFromChar = charFromFile - '0';
+			sudoku[row][column] = intFromChar;
+			// Debugging line
+			printf("%d |", sudoku[row][column]);
+		}
+		// Skip the two formatting characters at the end of each line
+		fgetc(inputFile);
+		fgetc(inputFile);
+		// Debugging line
+		printf("\n");
+	}
+	
 
+	// Close the file before terminating
+	fclose(inputFile);
 	return 0;
 }
