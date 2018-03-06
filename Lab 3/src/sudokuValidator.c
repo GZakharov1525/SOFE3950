cffb78d88ac06ff25ccbb1e6c4cb72244a36035c
GZakharov1525 <GZakharov1525@users.noreply.github.com>
2018-03-06 16:25:48 -0500

    time to give up spaghetti code

--- Lab 3/src/sudokuValidator.c
+++ Lab 3/src/sudokuValidator.c
@@ -7,7 +7,13 @@ int sudoku[9][9];
 
 int results[27];
 int position = 0;
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+//pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+struct arguments
+{
+	int row;
+	int col;
+};
 
 void* checkColumn(void* args)
 {
@@ -17,9 +23,9 @@ void* checkColumn(void* args)
 	// Cast void pointer to the data structure you need
 	//int* argPtr = args;
 	//int row = argPtr;
-	puts("checkpoint 0 \n");
+	//puts("checkpoint 0 \n");
 	int row = *((int *)args);
-	puts("checkpoint 1 \n");
+	//puts("checkpoint 1 \n");
 
 	for (int column = 0; column < 9; column++)
 	{
@@ -27,7 +33,7 @@ void* checkColumn(void* args)
 		//return value corresponding to an invalid solution.
 		// Arrays are zero based so must offset number by 1
 		int temp = sudoku[row][column];
-		puts("checkpoint 2 \n");
+		//puts("checkpoint 2 \n");
 		if (temp == 0)
 		{
 			// Ignore zero values
@@ -38,11 +44,11 @@ void* checkColumn(void* args)
 			//return 0;
 			//argPtr = 0;
 			//pthread_exit(argPtr);
-			pthread_mutex_lock(&mutex);
+			//pthread_mutex_lock(&mutex);
 			results[position] = 0;
 			position++;
-			pthread_mutex_unlock(&mutex);
-
+			//pthread_mutex_unlock(&mutex);
+			puts("--invalid column \n");
 			pthread_exit(0);
 		}
 		else
@@ -51,20 +57,20 @@ void* checkColumn(void* args)
 			//and should be added to list of encountered numbers.
 			check[temp - 1] = temp;
 		}
-		puts("checkpoint 3 \n");
+		//puts("checkpoint 3 \n");
 	}
 
 	// Valid solution, no duplicate numbers found
 	//return 1;
 	//argPtr = 1;
 	//pthread_exit(argPtr);
-	puts("checkpoint 4 \n");
-	pthread_mutex_lock(&mutex);
+	//puts("checkpoint 4 \n");
+	//pthread_mutex_lock(&mutex);
 	results[position] = 1;
 	position++;
-	pthread_mutex_unlock(&mutex);
+	//pthread_mutex_unlock(&mutex);
 
-	puts("exiting checkColumn \n");
+	//puts("exiting checkColumn \n");
 
 	pthread_exit(0);
 }
@@ -95,11 +101,11 @@ void* checkRow(void* args)
 			//return 0;
 			//argPtr = 0;
 			//pthread_exit(argPtr);
-			pthread_mutex_lock(&mutex);
+			//pthread_mutex_lock(&mutex);
 			results[position] = 0;
 			position++;
-			pthread_mutex_unlock(&mutex);
-
+			//pthread_mutex_unlock(&mutex);
+			puts("--invalid row \n");
 			pthread_exit(0);
 		}
 		else
@@ -114,12 +120,12 @@ void* checkRow(void* args)
 	//return 1;
 	//argPtr = 1;
 	//pthread_exit(argPtr);
-	pthread_mutex_lock(&mutex);
+	//pthread_mutex_lock(&mutex);
 	results[position] = 1;
 	position++;
-	pthread_mutex_unlock(&mutex);
+	//pthread_mutex_unlock(&mutex);
 
-	puts("exiting checkRow \n");
+	//puts("exiting checkRow \n");
 
 	pthread_exit(0);
 }
@@ -139,9 +145,16 @@ void* checkSubGrid(void* args)
 	int* thread_args = (int*)args;
 
 	// Used to retrieve arguments from pointer
-	int arg1 = thread_args[0];
-	int arg2 = thread_args[1];
+	//int arg1 = thread_args[0];
+	//printf("%d ", arg1);
+	//int arg2 = thread_args[1];
+	//printf("%d \n", arg1);
 
+	struct arguments* test = (struct arguments *) args;
+	int arg1 = test->row;
+	printf("%d ", arg1);
+	int arg2 = test->col;
+	printf("%d ", arg2);
 	// Variables to determine end index of for loops
 	//used to avoid re-calculating index in each iteration
 	//of the loop if using 'row <= startIndex + gridSize'.
@@ -167,11 +180,11 @@ void* checkSubGrid(void* args)
 				// Duplicate number has been found, invalid solution
 				//return 0;
 				//pthread_exit(argPtr);
-				pthread_mutex_lock(&mutex);
+				//pthread_mutex_lock(&mutex);
 				results[position] = 0;
 				position++;
-				pthread_mutex_unlock(&mutex);
-
+				//pthread_mutex_unlock(&mutex);
+				puts("INVALID SUBGRID");
 				pthread_exit(0);
 			}
 			else
@@ -187,10 +200,10 @@ void* checkSubGrid(void* args)
 	//return 1;
 	//argPtr = 1;
 	//pthread_exit(argPtr);
-	pthread_mutex_lock(&mutex);
+	//pthread_mutex_lock(&mutex);
 	results[position] = 1;
 	position++;
-	pthread_mutex_unlock(&mutex);
+	//pthread_mutex_unlock(&mutex);
 
 	puts("exited checkSubGrid \n");
 
@@ -233,25 +246,32 @@ int main(int argc, char* argv[])
 	//int results[27];
 	int thread_args[2];
 	int counter = 0;
+	int thread_vars[9] = { 0,1,2,3,4,5,6,7,8 };
+	int subgrid_vars[3] = { 0,1,2 };
+
+	struct arguments run;
 
 	puts("Creating row/col threads \n");
 
 	for (int i = 0; i < 9; i++)
 	{
+		//int temp = i;
+		//int* ptr = &i;
 		// Spawns a thread for each row and column
-		if (pthread_create(&thid[counter], NULL, checkColumn, i) != 0)
+		if (pthread_create(&thid[counter], NULL, checkColumn, &thread_vars[i]) != 0)
 		{
 			perror("pthread_create() error for columns");
 			exit(1);
 		}
-		puts("col thread created \n");
+		//puts("col thread created \n");
 		counter++;
-		if (pthread_create(&thid[counter], NULL, checkRow, i) != 0)
+		// pthread_create(&thid[counter], NULL, checkRow, (void *)&i or ptr)
+		if (pthread_create(&thid[counter], NULL, checkRow, &thread_vars[i]) != 0)
 		{
 			perror("pthread_create() error for rows");
 			exit(1);
 		}
-		puts("row thread created \n");
+		//puts("row thread created \n");
 		counter++;
 	}
 
@@ -259,34 +279,47 @@ int main(int argc, char* argv[])
 	// Spawns a thread for each subgrid
 	for (int grid = 0; grid <= 2; grid++)
 	{
+		//printf("grid: %d \n", grid);
 		for (int subgrid = 0; subgrid <= 2; subgrid++)
 		{
-			thread_args[0] = grid;
-			thread_args[1] = subgrid;
-			if (pthread_create(&thid[counter], NULL, checkSubGrid, &thread_args) != 0)
+			//printf("subgrid: %d \n", subgrid);
+			//thread_args[0] = grid;
+			//thread_args[1] = subgrid;
+			//pthread_create(&thid[counter], NULL, checkSubGrid, &thread_args)
+			//thread_args[0] = subgrid_vars[grid];
+			//thread_args[1] = subgrid_vars[subgrid];
+			run.row = subgrid_vars[grid];
+			run.col = subgrid_vars[subgrid];
+			if (pthread_create(&thid[counter], NULL, checkSubGrid, &run) != 0)
 			{
 				perror("pthread_create() error for subgrids");
 				exit(1);
 			}
-			puts("created subgrid thread \n");
+			//puts("created subgrid thread \n");
 			counter++;
 		}
 	}
 
 	// Takes values from pthread_exit and adds them to an array
 	//that contains all results from each thread.
-	//for (int i = 0; i < 27; i++)
-	//{
-	//	results[i] = pthread_join(thid[i], NULL);
-	//}
+	for (int i = 0; i < 27; i++)
+	{
+		//results[i] = pthread_join(thid[i], NULL);
+		pthread_join(thid[i], NULL);
+	}
 
 	puts("verifying solution \n");
+	for (int j = 0; j < 27; j++)
+	{
+		printf("%d |", results[j]);
+	}
+
 	// Verify that all results passed back from checks are valid
 	for (int verify = 0; verify < 27; verify++)
 	{
 		if (results[verify] == 0)
 		{
-			printf("Invalid solution.");
+			printf("Invalid solution. \n");
 			exit(1);
 		}
 	}
